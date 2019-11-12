43f0d1d7eae63a77657b4866617876a2b4ac5ce9
Sébastien Pflanzl <sv.pflanzl@gmail.com>
2019-11-12 12:46:24 +0100

    add some data and stuff

---
+++ data/lang-course-blank.js
@@ -0,0 +1,55 @@
+const LANG_COURSE_EXAMPLE_BLANK = {
+  img: "",
+  name: "",
+  blurb: ``,
+  technologies: ["", "", ""],
+  text: ``,
+  videoUrls: ["", "", ""],
+  courseSpaceInfo: ``,
+  teachers: [
+    {
+      img: "",
+      name: "",
+      text: "",
+    },
+    {
+      img: "",
+      name: "",
+      text: "",
+    }
+  ],
+  news: [
+    {
+      date: "",
+      text: ""
+    },
+    {
+      date: "",
+      text: ""
+    }
+  ],
+  offers: [
+    {
+      name: "",
+      price: 0,
+      hours: 0,
+      text: ``
+    },
+    {
+      name: "",
+      price: 0,
+      hours: 0,
+      text: ``
+    },
+  ],
+  testimonials: [
+    {
+      name: "",
+      text: ""
+    },
+    {
+      name: "",
+      text: ""
+    }
+  ]
+}
