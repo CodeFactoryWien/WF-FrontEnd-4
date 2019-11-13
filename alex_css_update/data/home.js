0f8816cd0a8d425058e72dd4931879908657869a
Alex <codefactory2222@hotmail.com>
2019-11-13 09:32:44 +0100

    CSS Updaste

---
+++ alex_css_update/data/home.js
@@ -0,0 +1,9 @@
+const HOME_CONTENT = {
+    img: "./img/landing_narrow.jpg",
+    name: "",
+    blurb: ``,
+    technologies: ["", "", ""],
+    text: ``,
+    videoUrls: ["", "", ""],
+    courseSpaceInfo: ``
+}
