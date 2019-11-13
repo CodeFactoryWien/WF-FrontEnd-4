b75412beae234a654a5d53fb6ad9f9ac0d5c3a82
Kranael <markus.kaspar93@gmail.com>
2019-11-13 13:40:56 +0100

    Version 0.5

--- News-Page/News-Page.js
+++ News-Page/News-Page.js
@@ -1,8 +1,12 @@
+
+// Generate Cards
+var counter = 0
+
 newslist.forEach(news => {
 
     let hook = document.getElementById("hook")
-    hook.innerHTML+=
-    `
+    hook.innerHTML +=
+        `
     <div class="col-lg-10">
         <div class="card">
             <div class="card-header">
@@ -12,17 +16,18 @@ newslist.forEach(news => {
                 <img class="card-img" src="./img/java.jpg" alt="Card image">
                 <div class="middlebox">
                     <div class="card-body">
-                        <ul class="list-group list-group-flush">
+                        <ul class="list-group list-group-flush info">
                             <li class="list-group-item">Course start: ${news.startdate}</li>
+                            <li class="list-group-item">Seats left: ${news.seats-news.seatstaken} from ${news.seats}</li>
                             <li class="list-group-item">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</li>
                         </ul>
                     </div>
-                    <a href="#!" class="btn btn-primary">Go somewhere</a>
                 </div>
                 <div class="rightbox">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">${news.price}</li>
+                    <ul class="list-group list-group-flush price">
+                        <li class="list-group-item">Price: ${news.price}€</li>
                     </ul>
+                    <a href="#!" class="btn btn-primary button">Go somewhere</a>
                 </div>
             </div>
             <div class="card-footer">
@@ -32,9 +37,27 @@ newslist.forEach(news => {
     </div>
     `
 
-    let pricehook = document.querySelectorAll("ul.list-group list-group-flush");
-    pricehook.innerHTML+=
-    `
-    <li class="list-group-item">Course start: ${news.rabattprice}</li>
-    `
+
+
+    // Discount
+    let pricehook = document.querySelectorAll("ul.list-group.list-group-flush.price");
+    let infohook = document.querySelectorAll("ul.list-group.list-group-flush.info");
+    
+
+    if (news.seats === news.seatstaken) {
+        infohook[counter].children[1].innerText = "Booked up"
+    }
+
+    if (news.hasOwnProperty("rabattprice")) {
+
+        let obj = document.createElement("li");
+        obj.classList.add("list-group-item");
+        obj.innerText = "Newprice: "+news.rabattprice+"€";
+
+        pricehook[counter].firstElementChild.style.textDecoration = "line-through";
+        pricehook[counter].appendChild(obj);
+    }
+
+    counter++;
+
 });
